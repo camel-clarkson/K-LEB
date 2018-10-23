ef348270c132c9d06a9cddb30eff8cae0d425dc7
camel <delabrcd@clarkson.edu>
2018-10-23 19:25:05 -0400

    updating to newest version from github

--- README.md
+++ README.md
@@ -7,16 +7,30 @@ It implements three key features:
   2) The ability to keep the event recording process specific
   3) The ability to monitor events periodically with a high resolution kernel timer
 
-To apply the module, run the following commands:
-	> make clean; make
-	> sudo insmod kleb.ko
-	> dmesg
-	> sudo mknod /dev/temp c <major number> 0
+To automatically apply the module, run: 
 
-To start the module, run the following bash command:
-	> sudo ./ioctl_start
+```
+sudo bash initialize.sh
+```
 
+
+To manually apply the module, run the following commands:
+	
+	
+	make clean; make
+	sudo insmod kleb.ko
+	dmesg
+	sudo mknod /dev/kleb c <major number> 0
+	
+
+To manually start the module, run the following bash command:
+	
+	sudo ./ioctl_start <counter> <umask> <timer delay> <./program path>
+	
+	
 To stop the module, run the following bash command:
-	> sudo ./ioctl_stop
-
+	
+	
+	sudo ./ioctl_stop
+	
 
--- ioctl_start.c
+++ ioctl_start.c
@@ -5,15 +5,18 @@
 #include <sys/ioctl.h>
 #include "kleb.h"
 
-int main(){
-	
+int main(int argc, char **argv){
+
+// ARGS: Counter, Umask, Timer Delay (in NS), 
+// EX: 0xc0 0x4f 100000
 	kleb_ioctl_args_t kleb_ioctl_args;
 	int pid = fork();
 	
 	if(pid == 0){ // child
 		sleep(1);
 		printf("my pid (child): %d\n", getpid());
-		execv("./test_perf", (char *[]){"james", NULL} );
+		//char *const args[] = {"-cvf", "tar-archive-test.tar", "large-file"}; 
+		execv(argv[4], (char *[]){"james", NULL} );
 	}else{
 		int fd = open(DEVICE_PATH, O_RDWR);
 		if(fd == -1){
@@ -23,11 +26,12 @@ int main(){
 					
 		kleb_ioctl_args.pid = pid;
 		printf("child pid: %d\n", pid);
-		kleb_ioctl_args.counter = 0xc0; // Other options: 0x020b, 0x412e
-		kleb_ioctl_args.counter_umask = 0x3F;
-		kleb_ioctl_args.delay_in_ns = 1E5L;
+		kleb_ioctl_args.counter = strtol(argv[1], NULL, 16); // Other options: 0x020b, 0x412e
+		kleb_ioctl_args.counter_umask = strtol(argv[2], NULL, 16);
+		kleb_ioctl_args.delay_in_ns = strtol(argv[3], NULL, 10);
 		kleb_ioctl_args.user_os_rec = 3;
-
+		
+		// printf("%x\n%x\n%llu\n", kleb_ioctl_args.counter, kleb_ioctl_args.counter_umask, kleb_ioctl_args.delay_in_ns);
 		char *msg = "Starting stuff";
 		ioctl(fd, IOCTL_START, &kleb_ioctl_args);
 
