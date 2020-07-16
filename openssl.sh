0454db53baa7d3bd37c847dac24cdf16cf039f9f
camel@clarkson.edu <camel@clarkson.edu>
2020-07-16 19:11:39 -0400

    Cleaning Demo

--- openssl.sh
+++
@@ -1,1 +0,0 @@
-echo "Q" | openssl s_client -connect google.com:443 -tls1_2
--- python_test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	chdir("../LR_v13_tfdeploy");
-  system("python3 lr_script_OC_testing_only_2.py");
-}
